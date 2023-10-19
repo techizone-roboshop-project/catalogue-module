5f758cef06cc45143cb0e62832c62167ab392e5a
sivakumarreddy <info@joindevops.com>
2023-10-19 07:49:32 +0530

    changed health check as part of new client requirement

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
     var stat = {
-        app: 'OKK',
+        app: 'OKKK',
         mongo: mongoConnected
     };
     res.json(stat);
