4adce034e750edcd3915f1851a871d926fffdc9b
sivakumarreddy <info@joindevops.com>
2023-10-26 07:22:12 +0530

    added libraries

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
     var stat = {
-        app: 'OK-1',
+        app: 'OK-2',
         mongo: mongoConnected
     };
     res.json(stat);
